8427b22a3be1f8d3264d09cb402e1f795e6e382a
Harshal Patil <HARSHAL.PATIL33@SVKMMUMBAI.ONMICROSOFT.COM>
2024-09-29 18:07:13 +0530

    done

---
+++ system_monitor.sh
@@ -0,0 +1,21 @@
+Week 4
+
+
+lab 2-
+
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/PubSub%20Qwik%20Start%20-%20Console/gsp096.sh
+
+sudo chmod +x gsp096.sh
+
+./gsp096.sh
+
+
+lab3-
+
+export API_KEY=AIzaSyCZHE95P-Z_lg_nzFkFP04LxODgt_R97Tk
+
+curl -LO raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Google%20Cloud%20Speech%20to%20Text%20API%20Qwik%20Start/quicklabgsp119.sh
+
+sudo chmod +x quicklabgsp119.sh
+
+./quicklabgsp119.sh
